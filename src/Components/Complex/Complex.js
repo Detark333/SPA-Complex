61ae1a8b84690d043da3c0a7f9e1e96e3bdf0c68
Detark333 <denis_73007@mail.ru>
2021-01-27 16:43:00 +0300

    Update list

--- src/Components/Complex/Complex.js
+++ src/Components/Complex/Complex.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import complex from './Complex.module.css'
+const namesCategories = ["Архитектура","Благоустройство", "Безопасность", "Инженерия", "Инфраструктура", "Транспортная доступность"]
 
 const Complex = () => {
     return <div style={{display: 'inline-flex', width: '100%',height: '100%'}}>
-        <div style={{display: 'flex', flexGrow: '1'}}>123</div>
+        <div style={{display: 'flex', flexGrow: '1',flexDirection: 'column',
+            justifyContent: 'center'}}>
+            <ul style={{paddingLeft: 0}}>
+                {namesCategories.map((item)=> {
+                return <li className={complex.liCategories}><span className={complex.spanCategories}>{item}</span></li>})}
+            </ul>
+        </div>
         <div style={{display: 'flex', flexGrow: '3', background: '#F0F0F0'}}>123</div>
         <div style={{display: 'flex', flexGrow: '3'}}>123</div>
     </div>
