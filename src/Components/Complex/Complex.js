cc2becabdf42b83ccbc49a0a4f50c1b1d8392353
Detark333 <denis_73007@mail.ru>
2021-02-04 01:19:21 +0400

    animation card

--- src/Components/Complex/Complex.js
+++ src/Components/Complex/Complex.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import complex from './Complex.module.css'
 import './Complex.module.css'
-import {NavLink, Route, Switch} from "react-router-dom";
+import {NavLink, Route} from "react-router-dom";
 import Block from "./Block";
 import architecture from './Svg/Architecture.svg'
 import safety from './Svg/Safety.svg'
@@ -11,14 +11,15 @@ import landscaping from './Svg/Landscaping.svg'
 import infrastructure from './Svg/Infrastructure.svg'
 import { CSSTransition } from 'react-transition-group';
 const namesCategories = [
-    {name: 'Архитектура', svg: <img src={architecture} alt="architecture" />, link: 'Architecture', text: 'Оригинальная архитектура жилого комплекса бизнес-класса «Первомайская» формирует современный стиль жизни', path: '/Complex/0'},
-    {name: "Благоустройство", svg: <img src={landscaping} alt="landscaping" />, link: 'Landscaping', text: 'Запроектированные большие окна, которые пропускают много солнечного света, наполнят Ваши квартиры теплотой и уютом', path: '/Complex/1'},
-    {name: "Безопасность", svg: <img src={safety} alt="safety" />, link: 'Safety', text: 'Современный двор европейского уровня - территория для детей, игр на свежем воздухе и вечерних', path: '/Complex/2'},
-    {name: "Инженерия", svg: <img src={engineering} alt="engineering" />, link: 'Engineering', text: 'Оригинальная архитектура жилого комплекса бизнес-класса «Первомайская» формирует современный стиль жизни', path: '/Complex/3'},
-    {name: "Инфраструктура", svg: <img src={infrastructure} alt="infrastructure" />, link: 'Infrastructure', text: 'Прекрасный вариант для тех, кто предпочитает жить в спокойном районе среди интеллигенции, но при этом чувствовать ритм мегаполиса', path: '/Complex/4'},
-    {name: "Транспортная доступность", svg: <img src={transport} alt="transport" />, link: 'Transport', text: 'Жилой комплекс «Первомайская» расположен в престижном Академическом районе', path: '/Complex/5'}]
+    {name: 'Архитектура', svg: <img style={{position: 'absolute', overflow: 'hidden'}} src={architecture} alt="architecture" />, link: 'Architecture', text: 'Оригинальная архитектура жилого комплекса бизнес-класса «Первомайская» формирует современный стиль жизни', path: '/Complex/0'},
+    {name: "Благоустройство", svg: <img style={{position: 'absolute', overflow: 'hidden'}} src={landscaping} alt="landscaping" />, link: 'Landscaping', text: 'Запроектированные большие окна, которые пропускают много солнечного света, наполнят Ваши квартиры теплотой и уютом', path: '/Complex/1'},
+    {name: "Безопасность", svg: <img style={{position: 'absolute', overflow: 'hidden'}} src={safety} alt="safety" />, link: 'Safety', text: 'Современный двор европейского уровня - территория для детей, игр на свежем воздухе и вечерних', path: '/Complex/2'},
+    {name: "Инженерия", svg: <img style={{position: 'absolute', overflow: 'hidden'}} src={engineering} alt="engineering" />, link: 'Engineering', text: 'Оригинальная архитектура жилого комплекса бизнес-класса «Первомайская» формирует современный стиль жизни', path: '/Complex/3'},
+    {name: "Инфраструктура", svg: <img style={{position: 'absolute', overflow: 'hidden'}} src={infrastructure} alt="infrastructure" />, link: 'Infrastructure', text: 'Прекрасный вариант для тех, кто предпочитает жить в спокойном районе среди интеллигенции, но при этом чувствовать ритм мегаполиса', path: '/Complex/4'},
+    {name: "Транспортная доступность", svg: <img style={{position: 'absolute', overflow: 'hidden'}} src={transport} alt="transport" />, link: 'Transport', text: 'Жилой комплекс «Первомайская» расположен в престижном Академическом районе', path: '/Complex/5'}]
 
 const Complex = () => {
+    const indexMassive = useRef(0);
     return <div className={complex.mainDiv}>
         <div className={complex.linkDiv}>
             <ul className={complex.ul}>
@@ -29,32 +30,47 @@ const Complex = () => {
         <div className={complex.informationDiv}>
             {namesCategories.map(({path}, index) => (
                 <Route key={path} exact path={path}>
-                    {({match}) => (
-                        <CSSTransition
+                    {({match}) => {
+                        return <CSSTransition
                             in={match != null}
                             timeout={300}
                             classNames={{
-                                enter: complex.pageEnter,
+                                enter: complex.pageEnter ,
                                 enterActive: complex.pageEnterActive,
                                 exit: complex.pageExit,
                                 enterExitActive: complex.pageExitActive,
                             }}
+                            onEntered={()=>{indexMassive.current = index; console.log(indexMassive.current)}}
                             unmountOnExit
                         >
                             <div className={complex.page}>
                                 <Block textP={namesCategories[index].name} text={namesCategories[index].text} />
                             </div>
                         </CSSTransition>
+                    }}
+                </Route>
+            ))}
+        </div>
+        <div>
+            {namesCategories.map(({path}, index) => (
+                <Route key={path} exact path={path}>
+                    {({match}) => (
+                            <div className={complex.imgDiv}>
+                                <CSSTransition
+                                    in={match != null}
+                                    timeout={500}
+                                    classNames={{
+                                        enter: complex.imgEnter,
+                                    }}
+                                    unmountOnExit
+                                >
+                                    {namesCategories[index].svg}
+                                </CSSTransition>
+                            </div>
                     )}
                 </Route>
             ))}
         </div>
-        <Switch>
-            <Route
-                path="/Complex/:number"
-                render={props => namesCategories[props.match.params.number].svg}
-            />
-        </Switch>
     </div>
 }
 
