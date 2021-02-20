69e6b1b3f874a72f1ac79a2a6c9b5cf52b95b615
Detark333 <denis_73007@mail.ru>
2021-02-20 15:51:25 +0400

    initial commit

--- src/Components/Penthouse/Penthouse.js
+++ src/Components/Penthouse/Penthouse.js
@@ -3,7 +3,14 @@ import penthouse from './Penthouse.module.css'
 
 const Penthouse = () => {
 
-    return <div className={penthouse.mainDiv}> </div>
+    return <div className={penthouse.mainDiv}>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
 }
 
 export default Penthouse
